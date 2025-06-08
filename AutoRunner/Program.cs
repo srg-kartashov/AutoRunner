055e22c36437ac89ecaeed815c6c9dc6960d6ee1
srg.kartashov <srg.kartashov@gmail.com>
2025-06-08 11:49:27 +0300

    add port

--- AutoRunner/Program.cs
+++ AutoRunner/Program.cs
@@ -10,6 +10,8 @@ namespace AutoRunner
 
             builder.Services.AddControllers();
 
+            builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
