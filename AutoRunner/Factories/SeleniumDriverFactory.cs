e08fc07c9f7475d72111c56d9f22ffa43054d361
srg-kartashov <srg.kartashov@gmail.com>
2025-07-20 16:55:26 +0300

    fix ChromeOptions

--- AutoRunner/Factories/SeleniumDriverFactory.cs
+++ AutoRunner/Factories/SeleniumDriverFactory.cs
@@ -9,8 +9,10 @@ namespace AutoRunner.Factories
         {
             var options = new ChromeOptions();
             options.AddArgument("--headless=new");
+            options.AddArgument("--window-size=1920,1080");
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-gpu");
+            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
 
             return new ChromeDriver(options);
         }
